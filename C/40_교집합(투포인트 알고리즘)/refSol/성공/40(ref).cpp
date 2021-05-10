b387fe735763fe61a7a511f6c0f346b90633ac00
Horingring <dldldksl@naver.com>
2021-05-10 12:47:32 +0900

    0510_Algorithm_Day22-2
    40. 교집합(투포인터 알고리즘)
    - 성공(강의 참고)
    : 알고보니 앞서 배운 정렬방식들(선택, 버블, 삽입)로는 시간초과가 난다고 한다.
    : 제한시간 내에 답을 구하기 위해서는 algorithm stl의 sort( )함수를 이용해야 했다.
     
    - sort(첫째 인자,  둘째 인자)
    : 해당 배열을 정렬(* 이 경우에는 오름차순)해준다.
    : 첫째 인자 - 배열의 포인터
    : 둘째 인자 - 배열의 포인터+배열의 크기
    ( * sort함수 정보 출처 - https://coding-factory.tistory.com/595 )

--- C/40_교집합(투포인트 알고리즘)/refSol/성공/40(ref).cpp
+++ C/40_교집합(투포인트 알고리즘)/refSol/성공/40(ref).cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include <algorithm>
 using namespace std;
 
 int main(){
 	int n, m, iA=0, iB=0, iC=0, p1, p2;  //교집합 구하기용 변수
-	int min, minIndex, temp;  //선택정렬용 변수
 
 	/* a배열 */
 	//입력
@@ -12,58 +12,29 @@ int main(){
 	for(int i=0; i<n; i++){
 		scanf("%d", &a[i]);
 	}
-	//선택정렬
-	for(int i=0; i<n-1; i++){
-		min=a[i];
-		minIndex=i;
-		for(int j=i+1; j<n; j++){
-			if(min>a[j]){
-				min=a[j];
-				minIndex=j;
-			}
-		}
-		if(min!=a[i]){
-			temp=a[i];
-			a[i]=min;
-			a[minIndex]=temp;
-		}
-	}
+	//정렬(by algorithm stl)
+	sort(a, a+n);  //  <----------바뀐 부분
 	
 	/* b배열 */
-	//입력
 	scanf("%d", &m);
 	int b[m]={0,};
 	for(int i=0; i<m; i++){
 		scanf("%d", &b[i]);
 	}
-	//선택정렬
-	for(int i=0; i<m-1; i++){
-		min=b[i];
-		minIndex=i;
-		for(int j=i+1; j<m; j++){
-			if(min>b[j]){
-				min=b[j];
-				minIndex=j;
-			}
-		}
-		if(min!=b[i]){
-			temp=b[i];
-			b[i]=min;
-			b[minIndex]=temp;
-		}
-	}
+	//정렬(by algorithm stl)
+	sort(b, b+m);  //  <----------바뀐 부분
 	
-	/* c배열(교집합의 배열) */
+	/* c배열(교집합 배열) */
 	int c[n+m] = {0,};
 	for(int i=0; i<n+m; i++){
 		c[i] = -2147000000;
 	}
 	
-	
 	/* 포인터 */
 	p1=a[iA];  // p1=a[0]
 	p2=b[iB];  // p2=b[0]
 	
+	/* 교집합 구하는 알고리즘 */
 	while(iA <= n && iB <= m){
 		if(p1==p2){
 			c[iC++]=p1;
